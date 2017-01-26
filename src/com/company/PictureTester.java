787defd545b84332e65dee429657f5077c0b25b4
Will YE <iamthenewlebron@gmail.com>
2017-01-25 20:47:21 -0500

    dfdfas

--- src/com/company/PictureTester.java
+++ src/com/company/PictureTester.java
@@ -78,7 +78,7 @@ public class PictureTester
     // uncomment a call here to run a test
     // and comment out the ones you don't want
     // to run
-    //testZeroBlue();
+    testZeroBlue();
     //testKeepOnlyBlue();
     //testKeepOnlyRed();
     //testKeepOnlyGreen();
